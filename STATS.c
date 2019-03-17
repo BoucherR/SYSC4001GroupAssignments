8269127dd7afe9c320926ed08de3a160ecffb6ac
BoucherR <36178025+BoucherR@users.noreply.github.com>
2019-03-17 03:04:37 -0400

    Got majority of code working
    Just need to add exit conditions to the forks

--- STATS.c
+++ STATS.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/shm.h>
+#include <semaphore.h>
+#include <errno.h>
+
+#define TRUE 1
+#define FALSE 0
 
 // Function that given an array, orders the two values largest to smallest
 void orderNumerically(int *array, int size) {
@@ -34,7 +39,6 @@ int main() {
 
     // Creating shared memory
     void *shared_memory = (void *)0;
-    struct shared_arrays *arrays;
     int shmid;
     shmid = shmget((key_t)1501, sizeof(int)*5, 0666 | IPC_CREAT);
 
@@ -48,15 +52,22 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    int *Q = malloc(sizeof(int)*5); // matrix that will be ordered
+    int* Q = shared_memory; // Assigning Q to shared memory, that each of the forks will modify
     Q[0] = 5;
     Q[1] = 6;
     Q[2] = 8;
     Q[3] = 2;
     Q[4] = 7;
 
-    // Assigning Q to shared memory, that each of the forks will modify
-    Q = shared_memory; 
+    // Starting Semaphore
+    errno = 0;
+    sem_t *sem;
+    sem = sem_open("/mysem", O_CREAT|O_EXCL, 0, 1);
+    sem_unlink("/mysem");
+    if (sem == SEM_FAILED) {
+        int err1 = errno;
+        fprintf(stderr, "sem_open() failed.  errno:%d\n", err1);
+    }
 
 	printf("--------------------- Program starting ---------------------\n\n");
 	
@@ -64,13 +75,19 @@ int main() {
 
     if (pid1 == 0) { // it's a child
         printf("This is child 1 with pid %d and ppid %d \n", getpid(), getppid());
-
-        int *pairOne = malloc(sizeof(int)*2);
-
         
-        pairOne[0] = Q[0];
-        pairOne[1] = Q[1];
-        orderNumerically(pairOne, 2);
+        do {
+            //wait
+            sem_wait(sem);
+
+            // critical section
+            orderNumerically(Q, 2);
+            printArray(Q, 5);
+
+            //signal
+            sem_post(sem);
+        } while(1);
+        printf("SHOULDNT BE HERE");
         
     }
     else { // second fork that will deal with the second row
@@ -79,10 +96,17 @@ int main() {
         if (pid2 == 0) { // it's a child
             printf("This is child 2 with pid %d and ppid %d \n", getpid(), getppid());
 
-            int *pairTwo = malloc(sizeof(int)*2);
-            pairTwo[0] = Q[1];
-            pairTwo[1] = Q[2];
-            orderNumerically(pairTwo, 2);
+            do {
+                //wait
+                sem_wait(sem);
+
+                // critical section
+                orderNumerically((Q+1), 2);
+                printArray(Q, 5);
+
+                //signal
+                sem_post(sem);
+            } while(1);
             
         }
         else { // third fork that will deal with the third row
@@ -91,10 +115,17 @@ int main() {
             if (pid3 == 0) { // it's a child
                 printf("This is child 3 with pid %d and ppid %d \n", getpid(), getppid());
 
-                int *pairThree = malloc(sizeof(int)*2);
-                pairThree[0] = Q[2];
-                pairThree[1] = Q[3];
-                orderNumerically(pairThree, 2);
+                do {
+                    //wait
+                    sem_wait(sem);
+
+                    // critical section
+                    orderNumerically((Q+2), 2);
+                    printArray(Q, 5);
+
+                    //signal
+                    sem_post(sem);
+                } while(1);
                 
             }
             else { // fourth fork that will deal with the fourth row
@@ -103,10 +134,17 @@ int main() {
                 if (pid4 == 0) { // it's a child
                     printf("This is child 4 with pid %d and ppid %d \n", getpid(), getppid());
 
-                    int *pairFour = malloc(sizeof(int)*2);
-                    pairFour[0] = Q[3];
-                    pairFour[1] = Q[4];
-                    orderNumerically(pairFour, 2);
+                    do {
+                        //wait
+                        sem_wait(sem);
+
+                        // critical section
+                        orderNumerically((Q+3), 2);
+                        printArray(Q, 5);
+
+                        //signal
+                        sem_post(sem);
+                    } while(1);
                 }
                 else // you're at the parent
                 {
@@ -118,5 +156,6 @@ int main() {
     }
 
     sleep(1);
+    sem_unlink("mysem");
     exit(0);
 }
